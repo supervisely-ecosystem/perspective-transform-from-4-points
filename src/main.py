de2ba5d4046d09e69b36971afbfe9bd785f5fa04
German Vorozhko <vorozhkog@icloud.com>
2022-11-29 18:20:05 +0000

    done

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
-from art import tprint
+import numpy as np
+import cv2
+import imutils
 
 # load ENV variables for debug
 # has no effect in production
@@ -8,9 +10,91 @@ load_dotenv("local.env")
 
 
 def main():
-    name = os.environ["context.userLogin"]
-    print("Hello World! This app is run by the user:")
-    tprint(name)
+    def order_points(pts):
+        rect = np.zeros((4, 2), dtype = "float32")
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect
+
+    def four_point_transform(image, pts):
+        # obtain a consistent order of the points and unpack them
+        # individually
+        rect = order_points(pts)
+        (tl, tr, br, bl) = rect
+        # compute the width of the new image, which will be the
+        # maximum distance between bottom-right and bottom-left
+        # x-coordiates or the top-right and top-left x-coordinates
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+
+        dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+        return warped
+    
+    # edge detection
+    image = cv2.imread(os.path.expanduser('~/work-apps/perspective-transform-from-4-points/test.jpg'))
+    # ratio = image.shape[0] / 500.0
+    # orig = image.copy()
+    # image = imutils.resize(image, height = 500)
+    # convert the image to grayscale, blur it, and find edges
+    # in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+    # show the original image and the edge detected image
+    print("STEP 1: Edge Detection")
+    cv2.imshow("Image", image)
+    cv2.imshow("Edged", edged)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    # show the contour (outline) of the piece of paper
+    print("STEP 2: Find contours of paper")
+    image_to_cntr = image.copy()
+    countered_img = cv2.drawContours(image_to_cntr, [screenCnt], -1, (0, 255, 0), 2)
+    cv2.imshow("Outline", countered_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    pts = np.array(screenCnt.reshape(4,2),dtype="float32")
+
+    warped = four_point_transform(image, pts)
+
+    cv2.imshow("Original", image)
+    cv2.imshow("Warped", warped)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":

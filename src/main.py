b8ae6befccde3cbb181f708a335ebe3e7cfe73ac
German Vorozhko <vorozhkog@icloud.com>
2022-12-06 13:17:44 +0000

    wip

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import numpy as np
 import cv2
 import imutils
-
 # load ENV variables for debug
 # has no effect in production
 load_dotenv("local.env")
@@ -15,22 +14,16 @@ def main():
         s = pts.sum(axis = 1)
         rect[0] = pts[np.argmin(s)]
         rect[2] = pts[np.argmax(s)]
-        # now, compute the difference between the points, the
-        # top-right point will have the smallest difference,
-        # whereas the bottom-left will have the largest difference
+
         diff = np.diff(pts, axis = 1)
         rect[1] = pts[np.argmin(diff)]
         rect[3] = pts[np.argmax(diff)]
         return rect
 
     def four_point_transform(image, pts):
-        # obtain a consistent order of the points and unpack them
-        # individually
         rect = order_points(pts)
         (tl, tr, br, bl) = rect
-        # compute the width of the new image, which will be the
-        # maximum distance between bottom-right and bottom-left
-        # x-coordiates or the top-right and top-left x-coordinates
+
         widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
         widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
         maxWidth = max(int(widthA), int(widthB))
@@ -39,28 +32,30 @@ def main():
         heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
         maxHeight = max(int(heightA), int(heightB))
 
-        dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
+        fin_tl_x, fin_tl_y = int(max(tl)), int(min(tl))
+        fin_tr_x, fin_tr_y = int(max(tl)) + int(widthB) - 1, int(min(tl))
+        fin_br_x, fin_br_y = int(max(tl)) + int(widthB) - 1, int(min(tl)) + int(widthB) - 1
+        fin_bl_x, fin_bl_y = int(max(tl)),int(min(tl)) + int(widthB) - 1
+
+
+        dst = np.array(
+		[fin_tl_x, fin_tl_y],
+		[fin_tr_x, fin_tr_y],
+		[fin_br_x, fin_br_y],
+		[fin_bl_x, fin_bl_y], dtype = "float32")
 
         M = cv2.getPerspectiveTransform(rect, dst)
-        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        warped = cv2.warpPerspective(src=image, M=M, dsize=(orig_wid, orig_height))
 
         return warped
     
-    # edge detection
-    image = cv2.imread(os.path.expanduser('~/work-apps/perspective-transform-from-4-points/test.jpg'))
-    # ratio = image.shape[0] / 500.0
-    # orig = image.copy()
-    # image = imutils.resize(image, height = 500)
-    # convert the image to grayscale, blur it, and find edges
-    # in the image
+    # image = cv2.imread('test.jpg')
+    image = cv2.imread('image1.jpg')
+    orig_height = image.shape[0]
+    orig_wid = image.shape[1]
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 75, 200)
-    # show the original image and the edge detected image
     print("STEP 1: Edge Detection")
     cv2.imshow("Image", image)
     cv2.imshow("Edged", edged)
@@ -72,7 +67,6 @@ def main():
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
     # loop over the contours
     for c in cnts:
-        # approximate the contour
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
         # if our approximated contour has four points, then we
@@ -89,13 +83,20 @@ def main():
     cv2.destroyAllWindows()
 
     pts = np.array(screenCnt.reshape(4,2),dtype="float32")
-
     warped = four_point_transform(image, pts)
+
+    detector = cv2.QRCodeDetector()
+    data, vertices_array, binary_qrcode = detector.detectAndDecode(countered_img)
+
+    if vertices_array is not None:
+        print("QRCode data:")
+        print(data)
+    else:
+        print("There is no QR code or program has failed to detect it it.") 
 
     cv2.imshow("Original", image)
     cv2.imshow("Warped", warped)
     cv2.waitKey(0)
-
 
 if __name__ == "__main__":
     main()
